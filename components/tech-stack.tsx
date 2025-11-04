72f603d7a76732c182e090cac114acccc84bc642
Tutku Ucan <tutku.dev@gmail.com>
2025-11-04 18:06:30 +0300

    initial commit

--- components/tech-stack.tsx
+++
@@ -1,5 +0,0 @@
-export default async function TechStack(){
-  const response: any = await fetch("https://skillicons.dev/icons?i=js,go,html,css,sass,react,vue,nodejs,nextjs,nuxtjs,mongodb,postgresql,redis")
-  const text:string = await response.text()
-  return text
-}
